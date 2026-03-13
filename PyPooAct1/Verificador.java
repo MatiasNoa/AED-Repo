daedd3e9d4b6ae38c75b3f41b05e23a5873651bb
MatiasNoa <matiaustin123@gmail.com>
2026-03-13 08:43:33 -0500

    Avance

---
+++ PyPooAct1/Verificador.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Verificador{
+    public static boolean esSobrePos(Rectangulo r1, Rectangulo r2){
+        return false;
+    }
+
+    public static boolean esJunto(Rectangulo r1, Rectangulo r2){
+        return false;
+    }
+
+    public static boolean esDisjunto(Rectangulo r1, Rectangulo r2){
+        return false;
+    }
+}
